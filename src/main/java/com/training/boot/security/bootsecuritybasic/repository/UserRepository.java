aa724bfbc000c90c7a7a664b67d8541c23ca0e27
Khemais MENZLI <menzli.khemais@gmail.com>
2024-05-05 21:51:56 +0200

    configure drop of database

--- src/main/java/com/training/boot/security/bootsecuritybasic/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.training.boot.security.bootsecuritybasic.repository;
-
-import com.training.boot.security.bootsecuritybasic.entities.DAOUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<DAOUser, Integer> {
-    DAOUser findByUsername(String name);
-}
